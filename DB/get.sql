0de392d0da43da7b00b8ae98cf6c0eaa501341f5
James Dunne <james.jdunne@gmail.com>
2013-02-01 12:00:45 -0600

    Optimized get query with index hint and LOOP JOIN. Added Guid for test code.

--- DB/get.sql
+++ DB/get.sql
@@ -1,21 +1,23 @@
-SELECT TOP 6000
-      i.exInstanceID, i.LoggedTimeUTC, i.SequenceNumber, i.IsHandled, i.ParentInstanceID, i.Message
+SELECT TOP 100
+      i.exInstanceID, i.LoggedTimeUTC, i.SequenceNumber, i.IsHandled, i.ParentInstanceID, i.CorrelationID, i.[Message]
     , app.ApplicationName, app.EnvironmentName
     , e.TypeName + ', ' + e.AssemblyName AS ExceptionType, e.exTargetSiteID, e.StackTrace
+    , ts.[FileName], ts.FileLineNumber, ts.FileColumnNumber, ts.TypeName, ts.MethodName
     , webapp.MachineName, webapp.ApplicationID, webapp.SiteName, webapp.PhysicalPath, webapp.VirtualPath
     , web.HttpVerb
     , requ.Scheme + '://' + requ.HostName + ':' + CONVERT(varchar(5), requ.PortNumber) + requ.AbsolutePath + requq.QueryString AS RequestURL
     , web.RequestHeadersCollectionID
     , web.AuthenticatedUserName
-FROM [dbo].[exInstance] i
-JOIN [dbo].[exException] e ON e.exExceptionID = i.exExceptionID
-JOIN [dbo].[exApplication] app ON app.exApplicationID = i.exApplicationID
+FROM [dbo].[exInstance] i WITH(INDEX(IX_exInstance_LoggedTimeUTC))
+INNER LOOP JOIN [dbo].[exException] e ON e.exExceptionID = i.exExceptionID
+LEFT JOIN [dbo].[exTargetSite] ts ON ts.exTargetSiteID = e.exTargetSiteID
+INNER JOIN [dbo].[exApplication] app ON app.exApplicationID = i.exApplicationID
 LEFT JOIN [dbo].[exContextWeb] web ON web.exInstanceID = i.exInstanceID
 LEFT JOIN [dbo].[exURLQuery] requq ON requq.exURLQueryID = web.RequestURLQueryID
 LEFT JOIN [dbo].[exURL] requ ON requ.exURLID = requq.exURLID
 LEFT JOIN [dbo].[exWebApplication] webapp ON webapp.exWebApplicationID = web.exWebApplicationID
-ORDER BY i.[LoggedTimeUTC] DESC, i.[SequenceNumber] DESC
-
+ORDER BY i.[LoggedTimeUTC] DESC, i.[SequenceNumber] DESC, i.exInstanceID DESC
+/*
 -- Get header collections
 SELECT TOP 1000
     kv.[exCollectionID]
@@ -25,3 +27,4 @@ SELECT TOP 1000
 FROM [dbo].[exCollectionKeyValue] kv
 JOIN [dbo].[exCollectionValue] v ON v.[exCollectionValueID] = kv.[exCollectionValueID]
 ORDER BY kv.[exCollectionID]
+*/
--- LorgMvcTest/Controllers/TestController.cs
+++ LorgMvcTest/Controllers/TestController.cs
@@ -12,22 +12,25 @@ namespace LorgMvcTest.Controllers
 #pragma warning disable 1998
         public async Task<ActionResult> Index()
         {
+            var correlationID = Guid.NewGuid();
             for (int i = 0; i < 100; ++i)
-                await this.Try(async () => { throw new NullReferenceException("Test"); });
+                await this.Try(async () => { throw new NullReferenceException("Test"); }, isHandled: true, correlationID: correlationID);
             return Content("");
         }
 
         public async Task<ActionResult> Bulk()
         {
+            var correlationID = Guid.NewGuid();
             for (int i = 0; i < 1000; ++i)
-                await this.Try(async () => { throw new Exception("Test"); });
+                await this.Try(async () => { throw new Exception("Test"); }, isHandled: true, correlationID: correlationID);
             return Content("");
         }
 
         public async Task<ActionResult> Inner()
         {
+            var correlationID = Guid.NewGuid();
             for (int i = 0; i < 500; ++i)
-                await this.Try(async () => { throw new Exception("Test", new Exception("Inner test")); });
+                await this.Try(async () => { throw new Exception("Test", new Exception("Inner test")); }, isHandled: true, correlationID: correlationID);
             return Content("");
         }
 
